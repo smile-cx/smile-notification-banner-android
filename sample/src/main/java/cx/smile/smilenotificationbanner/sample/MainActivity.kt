250fb96665295678f9f684f489ea56024e4dff9c
Michael Siddi <michael.siddi@covisian.com>
2025-12-15 13:55:51 +0100

    Fix sample app build and upgrade to AGP 8.7.3 and Gradle 8.9

--- sample/src/main/java/cx/smile/smilenotificationbanner/sample/MainActivity.kt
+++ sample/src/main/java/cx/smile/smilenotificationbanner/sample/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
-import cx.smile.smilenotificationbanner.BannerConfig
 import cx.smile.smilenotificationbanner.BannerPosition
 import cx.smile.smilenotificationbanner.BannerType
 import cx.smile.smilenotificationbanner.SmileBanner
